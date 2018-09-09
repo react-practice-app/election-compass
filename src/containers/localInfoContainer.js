035e20c8b0e2e4baae764d5f556202ada5a3561d
Ali Sattar <alirsattar@gmail.com>
2018-09-09 18:49:39 -0400

    Created unnecessarily long and complicated loop to match officeholder names with titles; props also now correctly passing down to component and displaying

--- src/containers/localInfoContainer.js
+++ src/containers/localInfoContainer.js
@@ -1,79 +1,91 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import LocalInfo from '../components/LocalInfo';
+import LocalInfo from "../components/LocalInfo";
 
 // import 'whatwg-fetch';
 
-import 'axios';
-import Axios from 'axios';
+import "axios";
+import Axios from "axios";
 
 class LocalInfoContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      googleInfo: {},
+      officials: [],
+      offices: [],
+      props: [],
+      officialsWithTitles: []
+    };
+  }
 
-    constructor(){
-        super();
-        this.state = {
-            googleInfo: {},
-            officials: [],
-            offices: [],
-            props: []
-        }
-    }
-    
-    componentDidMount(){
-        Axios.get('https://www.googleapis.com/civicinfo/v2/representatives?address=17660%2076th%20ct%20.%20Hialeah%20FL&key=AIzaSyBQmB4EGSCfePPlGmYGD-MUaLBsP49sP-Y')
-            .then((response) => {
-                // console.log(response);
-                // console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',this.state.googleInfo.name);
-                this.setState({
-                    googleInfo:     response.data,
-                    officials:      response.data.officials,
-                    offices:        response.data.offices
-                });
-                // this.mappingInfo();
-                // console.log('~~~~~~~~~~~~~~~~~~~~~',this.state.repInfo);
-                this.mappingInfo();
-            })
-            .catch(err => {
-                console.log(err);
-            })
-    }
-    mappingInfo() {
-        if (this.state.officials.length && this.state.offices.length) {
-            let propsCopy = [];
-            for(let i = 0; i < this.state.officials.length; i++){
-                if(this.state.officials[i] && this.state.offices[i]){
-                    propsCopy[i] = this.state.officials[i];
-                    propsCopy[i].title = this.state.offices[i].name;
-                } else {
-                    // console.log('NO MATCH FOR OFFICE',this.state.offices[i],this.state.officials[i]);
-                    continue;
-                }
+  componentDidMount() {
+    Axios.get(
+      "https://www.googleapis.com/civicinfo/v2/representatives?address=17660%2076th%20ct%20.%20Hialeah%20FL&key=AIzaSyBQmB4EGSCfePPlGmYGD-MUaLBsP49sP-Y"
+    )
+      .then(response => {
+        // console.log(response);
+        // console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',this.state.googleInfo.name);
+        this.setState({
+          googleInfo: response.data,
+          officials: response.data.officials,
+          offices: response.data.offices
+        });
+        // this.mappingInfo();
+        // console.log('~~~~~~~~~~~~~~~~~~~~~',this.state.repInfo);
+        this.mappingInfo();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  mappingInfo() {
+    if (
+      this.state.officials.length &&
+      this.state.offices.length &&
+      this.state.officialsWithTitles.length != this.state.officials.length
+    ) {
+      let officialsWithTitlesCopy = [];
+      for (let i = 0; i < this.state.officials.length; i++) {
+        officialsWithTitlesCopy[i] = this.state.officials[i];
+        for (let n = 0; n < this.state.offices.length; n++) {
+          for (
+            let j = 0;
+            j < this.state.offices[n].officialIndices.length;
+            j++
+          ) {
+            if (this.state.offices[n].officialIndices[j] === i) {
+              officialsWithTitlesCopy[i].title = this.state.offices[n].name;
             }
-            this.setState({
-                props: propsCopy
-            });
-                return (<LocalInfo office={this.state.offices.name} official={this.state.officials.name} />);
-        } else {
-            console.log(`CAN'T RUN MAPPINGINFO()`);
-            console.log('--------------this.state.officials: ',this.state.officials);
-            console.log('--------------this.state.officials: ',this.state.offices);
+          }
         }
+      }
+      this.setState({
+        officialsWithTitles: officialsWithTitlesCopy
+      });
+    } else {
+      console.log(`CAN'T RUN MAPPINGINFO()`);
+      console.log("--------------this.state.officials: ", this.state.officials);
+      console.log("--------------this.state.officials: ", this.state.offices);
     }
-    render() {
-        console.log(this.state.googleInfo);
-        return (
-            <div className="container">
-                <div className="row">
-                    <ul>
-                        <h1>PLACEHOLDER</h1>
-                        {/* {this.state.cardsMapped} */}
-                    </ul>
-                </div>
-            </div>
-        );
-    }   
-
-
+  }
+  render() {
+    let cards = this.state.officialsWithTitles.map((official, i) => {
+      return (
+        <LocalInfo key={i} office={official.title} official={official.name} />
+      );
+    });
+    return (
+      <div className="container">
+        <div className="row">
+          <ul>
+            {/* <h1>PLACEHOLDER</h1> */}
+            {cards}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default LocalInfoContainer;
